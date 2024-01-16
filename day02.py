c9c784fe418583833ab199d94185f6abdf78b178
aaboyss214 <aaboyss214@naver.com>
2024-01-16 14:40:28 +0900

    1/16 final prime

--- day02.py
+++ day02.py
@@ -2,15 +2,19 @@
 
 number=int(input('숫자를 입력하시오 = '))
 is_prime=True
-a=2
-while a<number:
-    if number%a==0:
-       is_prime=False
-       break
-    a=a+1
 
-
-if is_prime:
-   print(f'{number}은 소수입니다.')
+if number <2:
+    print(f'{number}은 소수가 아닙니다.')
 else:
-   print(f'{number}은 소수가 아닙니다.')
+    a=2
+    while a<number:
+        if number%a==0:
+           is_prime=False
+           break
+        a=a+1
+
+
+    if is_prime:
+       print(f'{number}은 소수입니다.')
+    else:
+       print(f'{number}은 소수가 아닙니다.')
