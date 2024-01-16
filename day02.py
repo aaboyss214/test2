11de9e18bd36fa0ba52988afb7996b6bee5833c7
aaboyss214 <aaboyss214@naver.com>
2024-01-16 15:37:11 +0900

    1/16

--- day02.py
+++ day02.py
@@ -1,18 +1,22 @@
 #소수를 판별하는 프로그램
 #타입 판별 함수 알기
 
-number=int(input('숫자를 입력하시오 = '))
-is_prime=True
+numbers=input('숫자를 입력하시오 = ').split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-if number <2:
-    print(f'{number}은 소수가 아닙니다.')
-else:
-    for i in range(2, number):
-        if number%i==0:
-           is_prime=False
-           break
+if n1 > n2:
+    n1, n2 = n2, n1
 
-    if is_prime:
-       print(f'{number}은 소수입니다.')
+for number in range(n1, n2 + 1):
+    is_prime=True
+
+    if number < 2:
+        pass
     else:
-       print(f'{number}은 소수가 아닙니다.')
+        for i in range(2, number):
+            if number % i == 0:
+               is_prime = False
+               break
+        if is_prime:
+            print(number, end=' ')
